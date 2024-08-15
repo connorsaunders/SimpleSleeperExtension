cfa66ca2e0d0150c1381f3615743cb46c9f7c404
connorsaunders <connorsaunders18@gmail.com>
2024-08-15 13:01:42 -0400

    + Comments, refactoring

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,4 @@
-//////////////////////////////////////////////////////////////////////////
-//                          Mutation Observer
-//////////////////////////////////////////////////////////////////////////
+// Mutation Observer
 window.addEventListener("load", function () {
     const observer = new MutationObserver(function (mutations) {
         observer.disconnect();
@@ -21,10 +19,7 @@ window.addEventListener("load", function () {
     observeDOM();
 });
 
-//////////////////////////////////////////////////////////////////////////
-//                       Background color changes
-//////////////////////////////////////////////////////////////////////////
-
+// Background color changes
 const style = document.createElement('style');
 style.textContent = `
     .in-game {
@@ -40,86 +35,81 @@ const styleSheet = document.createElement("style");
 styleSheet.type = "text/css";
 document.head.appendChild(styleSheet);
 
-//////////////////////////////////////////////////////////////////////////
-//                       
-//////////////////////////////////////////////////////////////////////////
-
+// Declare variables
 let retryCount = 0;
-const maxRetries = 10;
+const maxRetries = 5;
 let interval;
 
-//////////////////////////////////////////////////////////////////////////
-//                       
-//////////////////////////////////////////////////////////////////////////
+
+// Check scores function
 function checkScores() {
 
+    // Fetch both users (opponent and yourself)
     const users = document.querySelectorAll(".matchup-row .user");
+
+    // Fetch both scores for users (2 divs)
     const scores = document.querySelectorAll(".matchup-row .user .score");
-    const firstPlayerSection = document.querySelector(".player-section"); // Gets the first .player-section
-    const playerScoresInFirstSection = firstPlayerSection ? firstPlayerSection.querySelectorAll(".player-scoring .score") : [];
 
-    if (playerScoresInFirstSection.length === 18) {
+    // Fetch all players sections (opponent and your players grouped by position)
+    const allPlayers = document.querySelector(".player-section"); 
+    
+    // Filter above by scores only
+    const allPlayersScores = allPlayers ? allPlayers.querySelectorAll(".player-scoring .score") : [];
 
-        let maxDifference = 0;
+    // Declare max difference of 2
+    let maxDifference = 0;
 
-        for (let i = 0; i < playerScoresInFirstSection.length; i += 2) {
-            const score1 = parseFloat(playerScoresInFirstSection[i].textContent.trim()) || 0;
-            const score2 = parseFloat(playerScoresInFirstSection[i + 1].textContent.trim()) || 0;
-            maxDifference = Math.max(maxDifference, Math.abs(score1 - score2));
-        }
+    // Iterate through each player score
+    // score 1 = your score
+    // score 2 = opponent score
+    // maxDifference = the maximum difference between any 2 players of the same position
+    // Needed for conditional formatting
+    for (let i = 0; i < allPlayersScores.length; i += 2) {
+        const score1 = parseFloat(allPlayersScores[i].textContent.trim()) || 0;
+        const score2 = parseFloat(allPlayersScores[i + 1].textContent.trim()) || 0;
+        maxDifference = Math.max(maxDifference, Math.abs(score1 - score2));
+    }
 
-        for (let i = 0; i < playerScoresInFirstSection.length; i += 2) {
-            const score1Element = playerScoresInFirstSection[i];
-            const score2Element = playerScoresInFirstSection[i + 1];
+    // Iterate through each player score
+    // score1Element = player on your team's score
+    // score2Element = player on your opponents's score
+    // difference = difference between player of same positions scores
+    // Add intensity factor with difference * max difference
+    for (let i = 0; i < allPlayersScores.length; i += 2) {
+        const score1Element = allPlayersScores[i];
+        const score2Element = allPlayersScores[i + 1];
 
-            // Check if either score contains '-'
-            const isScore1Dash = score1Element.textContent.trim() === "-";
-            const isScore2Dash = score2Element.textContent.trim() === "-";
+        // Trim negatives for list of all positive differences
+        // REFACTOR
+        const isScore1Dash = score1Element.textContent.trim() === "-";
+        const isScore2Dash = score2Element.textContent.trim() === "-";
+        let score1 = isScore1Dash ? 0 : parseFloat(score1Element.textContent);
+        let score2 = isScore2Dash ? 0 : parseFloat(score2Element.textContent);
 
-            let score1 = isScore1Dash ? 0 : parseFloat(score1Element.textContent);
-            let score2 = isScore2Dash ? 0 : parseFloat(score2Element.textContent);
+        // Get difference
+        const difference = Math.abs(score1 - score2);
+        const intensity = difference / maxDifference * 0.15 + 0.05; 
 
-            const difference = Math.abs(score1 - score2);
-            const intensity = difference / maxDifference * 0.15 + 0.05;  // Adjust the intensity based on the difference, with a minimum of 0.05
+        // Get both players of the same positions' cards
+        const playerItem1 = score1Element.closest('.matchup-player-item');
+        const playerItem2 = score2Element.closest('.matchup-player-item');
 
-            const playerItem1 = score1Element.closest('.matchup-player-item');
-            const playerItem2 = score2Element.closest('.matchup-player-item');
+        playerItem1.style.borderRadius = '8px';
+        playerItem2.style.borderRadius = '8px';
 
-            if (isScore1Dash) {
-                resetBackgroundColor(playerItem1);
-            }
-            if (isScore2Dash) {
-                resetBackgroundColor(playerItem2);
-            }
-
-            if (score1 < score2) {
-                if (playerItem1 && !isScore1Dash) {
-                    playerItem1.style.backgroundColor = `rgba(255, 0, 0, ${intensity})`;
-                    playerItem1.style.borderRadius = '8px';
-                }
-                if (playerItem2 && !isScore2Dash) {
-                    playerItem2.style.backgroundColor = `rgba(0, 128, 0, ${intensity})`;
-                    playerItem2.style.borderRadius = '8px';
-                }
-            } else if (score1 > score2) {
-                if (playerItem1 && !isScore1Dash) {
-                    playerItem1.style.backgroundColor = `rgba(0, 128, 0, ${intensity})`;
-                    playerItem1.style.borderRadius = '8px';
-                }
-                if (playerItem2 && !isScore2Dash) {
-                    playerItem2.style.backgroundColor = `rgba(255, 0, 0, ${intensity})`;
-                    playerItem2.style.borderRadius = '8px';
-                }
-            } else {  // they are tied
-                if (playerItem1 && !isScore1Dash) {
-                    playerItem1.style.backgroundColor = `rgba(255, 255, 0, ${intensity})`;
-                    playerItem1.style.borderRadius = '8px';
-                }
-                if (playerItem2 && !isScore2Dash) {
-                    playerItem2.style.backgroundColor = `rgba(255, 255, 0, ${intensity})`;
-                    playerItem2.style.borderRadius = '8px';
-                }
-            }
+        // Compare positional players:
+        // Yours is winning:
+        if (score1 < score2) {
+                playerItem1.style.backgroundColor = `rgba(255, 0, 0, ${intensity})`;
+                playerItem2.style.backgroundColor = `rgba(0, 128, 0, ${intensity})`;
+        // Opponent is winning:
+        } else if (score1 > score2) {
+                playerItem1.style.backgroundColor = `rgba(0, 128, 0, ${intensity})`;
+                playerItem2.style.backgroundColor = `rgba(255, 0, 0, ${intensity})`;
+        // Tied:
+        } else { 
+                playerItem1.style.backgroundColor = `rgba(255, 255, 0, ${intensity})`;
+                playerItem2.style.backgroundColor = `rgba(255, 255, 0, ${intensity})`;
         }
     }
 
@@ -211,9 +201,7 @@ function checkScores() {
     }
 }
 
-//////////////////////////////////////////////////////////////////////////
-//                       
-//////////////////////////////////////////////////////////////////////////
+// Reset colors
 function resetBackgroundColor(element) {
     if (element) {
         element.style.backgroundColor = '';
